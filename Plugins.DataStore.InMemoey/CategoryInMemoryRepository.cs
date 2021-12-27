df77d3e3e469deaeee55d76a12311e961fc3bffd
Chirag <Chirag@HO-CHIRAG>
2021-12-27 22:29:06 +0530

    Add project files.

---
+++ Plugins.DataStore.InMemoey/CategoryInMemoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using CoreBusiness;
+using System.Collections.Generic;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.InMemoey
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> categories;
+        public CategoryInMemoryRepository()
+        {
+            //Add some default categories
+            categories = new List<Category>()
+            {
+                new Category{ CategoryId = 1, Name="Beverage",Description="Beverage"},
+                new Category{ CategoryId = 2, Name="Bakery",Description="Bakery"},
+                new Category{ CategoryId = 3, Name="Meat",Description="Meat"},
+            };
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return categories;
+        }
+    }
+}
